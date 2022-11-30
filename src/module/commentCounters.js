93dae90f0d97a100f15fd97c5ba3e6482404d7df
The-Normal-One1 <manermidem@gmail.com>
2022-11-30 17:17:22 +0300

    Add comment-counter for the comment lists

---
+++ src/module/commentCounters.js
@@ -0,0 +1,8 @@
+const commentCounter = () => {
+    let noOfComments = 0;
+    const commentList = document.querySelector('.commentContainer');
+    noOfComments = commentList.childElementCount;
+    return noOfComments;
+  };
+
+  export default commentCounter;
